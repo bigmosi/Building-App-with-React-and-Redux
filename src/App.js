4e9cf5dc7498132191d2ad1c15dc6e4680ff7ac9
kinyeramos2021 <kinyeramo@gmail.com>
2021-08-31 02:16:03 +0300

    building react components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
+import Header from "./components/common/Header";
+import PageNotFound from "./components/PageNotFound";
+
+
+function App() {
+    return (
+        <div className="container-fluid">
+            <Header />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route  path="/about" component={AboutPage} />
+              <Route component={PageNotFound}/>
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
