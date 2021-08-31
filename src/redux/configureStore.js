1c0c0f5340b5ef0b5da4e2da862854fbcbd1df19
kinyeramos2021 <kinyeramo@gmail.com>
2021-09-01 00:04:18 +0300

    connect container component and mapDispatchToProps

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-import {createStore, applyMiddleware, compose} from "redux";
-import rootReducer from "../reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 
 export default function configureStore(initialState) {
-    const composeEnhanchers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-    return createStore(
-        rootReducer,
-        initialState,
-        composeEnhanchers(applyMiddleware(reduxImmutableStateInvariant()))
-    );
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
 }
